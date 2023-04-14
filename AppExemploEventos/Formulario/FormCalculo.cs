aeaadecfc9f0f5cad362ecee7abad8c16190dbff
Tiago Rafael <37108878+tegerafael@users.noreply.github.com>
2023-04-13 22:04:43 -0400

    AppExemploEventos

---
+++ AppExemploEventos/Formulario/FormCalculo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExemploEventos.Formulario
+{
+    public partial class FormCalculo : Form
+    {
+        public FormCalculo()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            double valor1, valor2, total;
+            valor1 = Convert.ToDouble(txtValor1.Text);
+            valor2 = Convert.ToDouble(txtValor2.Text);
+
+            if (valor2 == 0)
+            {
+                MessageBox.Show("Não é possível dividir por zero!", "Calcular Divisão", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtValor2.Clear();
+                txtValor2.Select();
+            } else {
+                total = valor1 / valor2;
+                txtResultado.Text = total.ToString();
+            }
+        }
+
+        private void btNovo_Click(object sender, EventArgs e)
+        {
+            txtValor1.Clear();
+            txtValor2.Clear();
+            txtResultado.Clear();
+        }
+
+        private void txtValor1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                txtValor2.Select();
+            }
+            if ((char.IsDigit(e.KeyChar) || ((e.KeyChar) == ',') || (e.KeyChar == 13)) == false)
+            {
+                e.Handled = true;
+                MessageBox.Show("Digite só números!");
+                txtValor1.Clear();
+                txtValor1.Select();
+            }
+        }
+    }
+}
---
+++ AppExemploEventos/Formulario/FormMensagem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExemploEventos.Formulario
+{
+    public partial class FormMensagem : Form
+    {
+        public FormMensagem()
+        {
+            InitializeComponent();
+        }
+
+        private void btAlerta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem da caixa", "Título da caixa", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem da caixa", "Título da caixa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btErro_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem da caixa", "Título da caixa", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btPergunta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem da caixa", "Título da caixa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+
+        private void btRespostas_Click(object sender, EventArgs e)
+        {
+            DialogResult resp;
+            string pergunta = "Palmeiras tem mundial?";
+            string titulo = "ADS 2022-2";
+            string resposta;
+            resp = MessageBox.Show(pergunta, titulo, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (resp == DialogResult.Yes)
+            {
+                resposta = "Sim! O palmeiras tem mundial!";
+                MessageBox.Show(resposta, titulo, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                resposta = "Não! O palmeiras não tem mundial!";
+                MessageBox.Show(resposta, titulo, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
